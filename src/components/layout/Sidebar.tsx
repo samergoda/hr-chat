51cacd9edf1dca4baad19e3db454b4e4b043b65d
samer <judosamer555@gmail.com>
2025-11-02 07:51:46 +0200

    fixed/side nav height om desktop and mobile

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -46,7 +46,7 @@ export default function Sidebar() {
       <aside
         id="sidebar"
         className={cn(
-          'fixed left-0 top-0 h-full md:h-auto z-40  w-64 border-r bg-gray-100 p-4 transition-transform',
+          'fixed left-0 top-0 h-full md:h-screen z-40  w-64 border-r bg-gray-100 p-4 transition-transform',
 
           open ? 'translate-x-0' : '-translate-x-full',
 
